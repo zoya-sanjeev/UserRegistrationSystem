65667b24e1499178abf61036e8f3a760e20f72c3
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-13 22:41:15 +0530

    REFACTORED: used lambda expression in validateEmail

--- lib/src/main/java/UserRegistrationSystem/UserRegistration.java
+++ lib/src/main/java/UserRegistrationSystem/UserRegistration.java
@@ -33,11 +33,11 @@ public class UserRegistration {
 	
 	public static boolean validateEmail(String email) throws UserRegistrationException {
 		try {
-		String emailValidation="^[a-zA-Z]{1,}([\\.\\_\\-\\+]?[a-zA-Z0-9]+)@[a-z0-9]+[.][a-z]+[.]?[a-z]{2,3}?$";
-		if(UserRegistration.validateInput.validate(emailValidation , email))
-			return true;
-		else
-			throw new UserRegistrationException("Enter email in proper format", ExceptionType.EMAIL_INVALID);
+			String emailValidation="^[a-zA-Z]{1,}([\\.\\_\\-\\+]?[a-zA-Z0-9]+)@[a-z0-9]+[.][a-z]+[.]?[a-z]{2,3}?$";
+			if(UserRegistration.validateInput.validate(emailValidation , email))
+				return true;
+			else
+				throw new UserRegistrationException("Enter email in proper format", ExceptionType.EMAIL_INVALID);
 		}catch(NullPointerException e) {
 			throw new UserRegistrationException("Enter name in proper format",ExceptionType.EMAIL_NULL);
 		}
